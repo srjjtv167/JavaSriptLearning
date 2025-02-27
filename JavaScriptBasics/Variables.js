c844fc89f96de16c498657f582e00712bba5f28b
srjjtv167 <104966220+srjjtv167@users.noreply.github.com>
2025-02-27 12:37:13 +0000

    Variable added in the file

---
+++ JavaScriptBasics/Variables.js
@@ -0,0 +1,11 @@
+const finalValue = 12345
+let firstName = "Suraj"
+var lastName = "Jatav"
+let middleName;
+age = 20
+
+
+//finalValue = 11111 Can't change the value of constant variables
+firstName = "Srj"
+
+console.table([finalValue, firstName, middleName, lastName, age])
