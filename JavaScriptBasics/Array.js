c0846b6d40b853078f8086dc2f78e122e9fd6577
srjjtv167 <104966220+srjjtv167@users.noreply.github.com>
2025-02-28 13:23:23 +0000

    array file added

---
+++ JavaScriptBasics/Array.js
@@ -0,0 +1,27 @@
+// let arrNumber = [3,3,4,5,6,3,231,21,3]
+// console.log(arrNumber)
+
+// arrNumber.push(100)
+// console.log(arrNumber)
+
+// arrNumber.pop()
+// console.log(arrNumber)
+
+// let arrSliced = arrNumber.slice(1,4)
+// console.log(arrSliced);
+
+// let arrSpliced = arrNumber.splice(1,4)
+// console.log(arrSpliced);
+
+
+//let anotherArray = [9,8,7]
+//let arrConcatinated = arrNumber.concat(anotherArray)
+//  or
+// let arrConcatinated = [...arrNumber, ...anotherArray]
+
+// console.log(arrConcatinated)
+
+
+//flat property
+let arrNumber = [2,3,5,[34,43,33], 1,3,5,7,[21,12,32,[1,0,8],2]]
+console.log(arrNumber.flat(Infinity))
