0f20721dd6dcd51115da8e609716ff238220206c
srjjtv167 <104966220+srjjtv167@users.noreply.github.com>
2025-02-28 12:09:13 +0000

    date and time file added

---
+++ JavaScriptBasics/DateAndTime.js
@@ -0,0 +1,7 @@
+let date = new Date()
+console.log(date);
+console.log(typeof date);
+console.log(date.toDateString());
+console.log(date.getDay());
+console.log(date.getHours());
+console.log(date.toLocaleDateString());
